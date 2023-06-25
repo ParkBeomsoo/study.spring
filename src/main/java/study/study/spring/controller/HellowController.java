f23f978bfe7cea6c1d0522d5144342a4d62fdb6d
ParkBeomsoo <68089887+ParkBeomsoo@users.noreply.github.com>
2023-06-25 22:52:31 +0900

    feat: static, mvc 테스트 코드 추가

--- src/main/java/study/study/spring/controller/HellowController.java
+++ src/main/java/study/study/spring/controller/HellowController.java
@@ -3,6 +3,7 @@ package study.study.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HellowController {
@@ -11,5 +12,11 @@ public class HellowController {
     public String hello(Model model){
         model.addAttribute("data", "hellow");
         return "hellow";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
